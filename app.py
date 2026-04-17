785de425720a868210d283f4fe0d97019b5be77e
nandan9632 <nandantn25@gmail.com>
2026-04-18 01:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# Metric
+REQUEST_COUNT = Counter('app_requests_total', 'Total Requests')
+
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
     return "Music Detector Running!"
+
+# ✅ THIS IS THE IMPORTANT PART
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
